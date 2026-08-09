486efb1fb0fd5b6e913d6692facf84463699cc25
HugoEdmoundo <hugoedmoundo@gmail.com>
2026-08-09 16:59:05 +0700

    fix: bulletproof manual vercel adapter

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,33 @@
-import { getRequestListener } from '@hono/node-server'
 import app from '../src/app'
 
-const listener = getRequestListener(app.fetch)
-
 module.exports = async function (req: any, res: any) {
-  return listener(req, res)
-}
-module.exports.config = {
-  api: {
-    bodyParser: false,
-  },
+  try {
+    const protocol = req.headers['x-forwarded-proto'] || 'https'
+    const host = req.headers['x-forwarded-host'] || req.headers.host || 'localhost'
+    const url = `${protocol}://${host}${req.url}`
+
+    const init: RequestInit = {
+      method: req.method,
+      headers: req.headers as any,
+    }
+
+    if (req.method !== 'GET' && req.method !== 'HEAD' && req.body) {
+      init.body = typeof req.body === 'string' ? req.body : JSON.stringify(req.body)
+    }
+
+    const request = new Request(url, init)
+    const response = await app.fetch(request)
+
+    res.status(response.status)
+    response.headers.forEach((val, key) => res.setHeader(key, val))
+
+    if (response.body) {
+      const arrayBuffer = await response.arrayBuffer()
+      res.send(Buffer.from(arrayBuffer))
+    } else {
+      res.send('')
+    }
+  } catch (err: any) {
+    res.status(500).json({ detail: 'Adapter Error', message: err.message, stack: err.stack })
+  }
 }
