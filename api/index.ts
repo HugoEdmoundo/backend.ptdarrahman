5e4026111058f77136694f541134b27381b8e709
HugoEdmoundo <hugoedmoundo@gmail.com>
2026-08-09 16:51:06 +0700

    fix: use hono node-server adapter

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,13 @@
-import { handle } from 'hono/vercel'
+import { getRequestListener } from '@hono/node-server'
 import app from '../src/app'
 
-module.exports = handle(app)
+const listener = getRequestListener(app.fetch)
+
+module.exports = async function (req: any, res: any) {
+  return listener(req, res)
+}
+module.exports.config = {
+  api: {
+    bodyParser: false,
+  },
+}
