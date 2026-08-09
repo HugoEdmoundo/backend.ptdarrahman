e844b124ea4b9b2367a7f28bfc0ac3a1d294056b
HugoEdmoundo <hugoedmoundo@gmail.com>
2026-08-09 16:44:03 +0700

    test: commonjs vercel handler

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
-export default function handler(req: any, res: any) {
+module.exports = function (req: any, res: any) {
   res.status(200).json({
-    message: 'Hello World from Native Node! If you see this, Hono was the problem.',
+    message: 'Hello World from Native Node with module.exports! If you see this, ESM/TypeScript was the problem.',
     url: req.url
   })
 }
