90c126d2652e26a02ceb205fbd3358cebc5d61f0
HugoEdmoundo <hugoedmoundo@gmail.com>
2026-08-09 14:24:32 +0700

    fix: replace hono/bun serveStatic with node fs handler and fix Vercel catch-all routing

--- api/[[route]].ts
+++
@@ -1,8 +0,0 @@
-import { handle } from 'hono/vercel'
-import { Hono } from 'hono'
-import app from '../src/app'
-
-const vercelApp = new Hono().basePath('/api')
-vercelApp.route('/', app)
-
-export default handle(vercelApp)
