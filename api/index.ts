9fab339c17aaf56bbf9d552d7a66e81650c0424b
HugoEdmoundo <hugoedmoundo@gmail.com>
2026-08-09 17:30:11 +0700

    fix: vercel deployment

--- api/index.ts
+++ api/index.ts
@@ -1,40 +1,4 @@
-// Dynamic import inside handler to catch initialization errors
-module.exports = async function (req: any, res: any) {
-  try {
-    const protocol = req.headers['x-forwarded-proto'] || 'https'
-    const host = req.headers['x-forwarded-host'] || req.headers.host || 'localhost'
-    const url = `${protocol}://${host}${req.url}`
+import { handle } from 'hono/vercel'
+import app from '../src/app'
 
-    const init: RequestInit = {
-      method: req.method,
-      headers: req.headers as any,
-    }
-
-    if (req.method !== 'GET' && req.method !== 'HEAD' && req.body) {
-      init.body = typeof req.body === 'string' ? req.body : JSON.stringify(req.body)
-    }
-
-    let appModule;
-    try {
-      appModule = require('../src/app')
-    } catch (reqErr: any) {
-      throw new Error(`Require failed: ${reqErr.message}`)
-    }
-    const app = appModule.default || appModule
-
-    const request = new Request(url, init)
-    const response = await app.fetch(request)
-
-    res.status(response.status)
-    response.headers.forEach((val, key) => res.setHeader(key, val))
-
-    if (response.body) {
-      const arrayBuffer = await response.arrayBuffer()
-      res.send(Buffer.from(arrayBuffer))
-    } else {
-      res.send('')
-    }
-  } catch (err: any) {
-    res.status(500).json({ detail: 'Adapter Error', message: err.message, stack: err.stack })
-  }
-}
+export default handle(app)
